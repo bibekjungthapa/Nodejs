9eb6eb17082beff40d650c03596276806976eb76
Bibek Thapa <thapabibek5555@gmail.com>
2024-07-30 07:07:22 +0545

    Final SetUp

--- models/task.model.js
+++ models/task.model.js
@@ -1,21 +1,4 @@
-
-
-
 import mongoose from "mongoose";
 
-const taskModel = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  { timestamps: true }
-);
 
-const task = mongoose.model("TaskModel", taskModel);
 export default task;
--- mongo/dbConnection.js
+++ mongo/dbConnection.js
@@ -3,6 +3,6 @@ export const dbConnect = () => {
 mongoose.set('strictQuery', true);
 
   mongoose.connect(process.env.MONGO_URL, () => {
-    console.log("database connected successfully");
+    console.log("Database connected successfully");
   });
 };
