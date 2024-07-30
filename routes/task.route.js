a612856ae44493aa29b3a53470101179eb7719fa
Bibek Thapa <thapabibek5555@gmail.com>
2024-07-30 07:13:44 +0545

    route setup

--- routes/task.route.js
+++ routes/task.route.js
@@ -1,11 +1,5 @@
 import { Router } from "express";
 
-import {
-  createTask,
-} from "../controllers/task.controller.js";
-const router = Router();
-
-router.post("/create", createTask);
 
 
 export default router;
