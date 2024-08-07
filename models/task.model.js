ccfc2bcee0b5c475c95ce7d637061cc2ab3bace5
Bibek Thapa <thapabibek5555@gmail.com>
2024-08-07 11:07:48 +0545

    this is the simple create api

--- models/task.model.js
+++ models/task.model.js
@@ -1,4 +1,18 @@
 import mongoose from "mongoose";
 
+const taskModel = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
+const task = mongoose.model("TaskModel", taskModel);
 export default task;
